825a568e89900b3ad4f8121ac10e4cf7cc0e1c4a
Bennie Thuisman <ronald.demunnik@bax-shop.nl>
2025-12-04 10:11:29 +0100

    typo

--- js/ui.js
+++ js/ui.js
@@ -64,6 +64,6 @@ export function updateWelcomeMessage() {
     const welcomeTitleEl = document.getElementById('welcome-title');
     if (welcomeTitleEl) {
         const firstName = currentUser.firstName || currentUser.name.split(' ')[0];
-        welcomeTitleEl.textContent = `Hoi ${firstName}! Waar kan ik je van vandaag helpen?`;
+        welcomeTitleEl.textContent = `Hoi ${firstName}! Waar kan ik je van vandaag mee helpen?`;
     }
 }
